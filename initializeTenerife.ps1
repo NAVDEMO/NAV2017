cda439df1e5c8658c295b1d830dea691624658ef
Freddy Kristiansen <freddyk@microsoft.com>
2016-12-20 02:12:30 +0100

    x

--- initializeTenerife.ps1
+++ initializeTenerife.ps1
@@ -62,7 +62,7 @@ Log("Machine Name is $MachineName")
 # Update RTM files
 $date = (Get-Date -Date "2016-12-11 00:00:00Z").ToUniversalTime()
 $PatchPath = $ScriptPath.SubString(0,$ScriptPath.LastIndexOf('/')+1)
-#PatchFileIfNecessary -date $date -baseUrl $PatchPath -path "DEMO/Initialize/install.ps1"
+PatchFileIfNecessary -date $date -baseUrl "${PatchPath}Tenerife/" -path "DEMO/New Developer Experience/install.ps1"
 
 if ($VMAdminUsername -eq "") {
     Log("Restart computer and stop installation")
