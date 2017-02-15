114f27aa7449ca95743b435355cb74c9341da283
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2017-02-15 15:32:04 +0100

    remove mht

--- initializeTenerife.ps1
+++ initializeTenerife.ps1
@@ -68,7 +68,6 @@ Log("Machine Name is $MachineName")
 $date = (Get-Date -Date "2017-02-16 00:00:00Z").ToUniversalTime()
 $PatchPath = $ScriptPath.SubString(0,$ScriptPath.LastIndexOf('/')+1)
 PatchFileIfNecessary -date $date -baseUrl "${PatchPath}Tenerife/" -path "DEMO/New Developer Experience/install.ps1"
-PatchFileIfNecessary -date $date -baseUrl "${PatchPath}Tenerife/" -path "DEMO/Welcome to Microsoft Dynamics NAV Tenerife Developer Preview.mht"
 
 if ($VMAdminUsername -eq "") {
     Log("Restart computer and stop installation")
